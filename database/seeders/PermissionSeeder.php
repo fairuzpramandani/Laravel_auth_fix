c1c8f1d967f9ce9e889be4f460fef957dbb1ebb3
fairuzpramandani <152507112+fairuzpramandani@users.noreply.github.com>
2025-08-28 21:15:47 +0700

    Add files via upload

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'view dashboard']);
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'edit users']);
+        Permission::create(['name' => 'delete users']);
+
+        // create roles and assign existing permissions
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $adminRole->givePermissionTo(Permission::all());
+        $userRole->givePermissionTo('view dashboard');
+    }
+}
